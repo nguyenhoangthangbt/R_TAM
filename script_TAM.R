1acbf26ac6e30da073a9002974ed6ba2e99463a0
nguyenhoangthangbt <44111898+nguyenhoangthangbt@users.noreply.github.com>
2025-06-23 13:00:42 +0700

    t

--- script_TAM.R
+++ script_TAM.R
@@ -1,1 +1,9 @@
 print("hello R with Github")
+
+# Generate x and y values
+data_x <- 1:100
+data_y <- 2 * sin(data_x)
+
+# Plot the data
+plot(data_x, data_y, type = "l", col = "blue", lwd = 2,
+     main = "Plot of y = 2*sin(x)", xlab = "x", ylab = "y")
